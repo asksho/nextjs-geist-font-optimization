d8ccfd90267e3111fb34a4e1e37b4347261e5dc5
asksho <saanmiretail@gmail.com>
2025-05-26 11:29:41 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1748239180952

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_fbd21a62._.js",
+  "static/chunks/src_7e710e3c._.js"
+],
+    source: "dynamic"
+});
